aeb369481f37b73e6dae5cb95347fc9a03455f57
lhb1032352370 <1032352370@qq.com>
2019-12-25 15:57:30 +0800

    ignore .catch & dist

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Button from './button'
+
+new Vue({
+  el: '#app',
+  components: {
+    'm-button': Button
+  }
+});
---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="m-button">提交</button>
+</template>
+
+<script>
+  export default {
+    name: "button"
+  }
+</script>
+
+<style scoped lang="scss">
+  .m-button {
+    padding: 0 1em;
+    height: var(--button-height);
+    font-size: var(--font-size);
+    border: 1px solid var(--border-color);
+    border-radius: var(--border-radius);
+    background-color: var(--button-bg);
+    color: var(--color);
+
+    &:hover {
+      background-color: var(--button-active-bg);
+      border-color: var(--border-hover-color);
+      color: var(--color-hover);
+    }
+  }
+</style>
