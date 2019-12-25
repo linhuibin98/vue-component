b265079aa8340b6934cd3bd2e603bfef3703bf62
lhb1032352370 <1032352370@qq.com>
2019-12-25 18:03:04 +0800

    add button icon

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,27 @@
 <template>
-  <button class="m-button">提交</button>
+  <button class="m-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <span class="content">
+      <slot></slot>
+    </span>
+  </button>
 </template>
 
 <script>
   export default {
-    name: "button"
+    name: "button",
+    props: {
+      icon: String,
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(value) {
+          return value === 'left' || value === 'right';
+        }
+      }
+    }
   }
 </script>
 
@@ -17,11 +34,36 @@
     border-radius: var(--border-radius);
     background-color: var(--button-bg);
     color: var(--color);
+    line-height: var(--button-height);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
 
     &:hover {
       background-color: var(--button-active-bg);
       border-color: var(--border-hover-color);
       color: var(--color-hover);
+    }
+
+    > .icon {
+      order: 1;
+      width: 1em;
+      height: 1em;
+    }
+    > .content {
+      order: 2;
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+      }
+
+      > .content {
+        order: 1;
+      }
+
     }
   }
 </style>
