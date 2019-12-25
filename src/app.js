7cca6be8d13ef3d75b9247c95ebbb46173520c9f
lhb1032352370 <1032352370@qq.com>
2019-12-25 22:02:47 +0800

    添加测试用例,并使用chai插件chai-spies测试函数是否被调用

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import ButtonGroup from './buttonGroup'
+import chai from 'chai'
+import spies from 'chai-spies'
+
+chai.use(spies)
+const expect = chai.expect
 
 new Vue({
   el: '#app',
@@ -13,3 +18,137 @@ new Vue({
     'm-button-group': ButtonGroup
   }
 });
+
+// 单元测试
+{
+  const Constructor = Vue.extend(Button)
+
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+
+  vm.$mount()
+
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+
+  expect(href).to.eq('#i-setting')
+
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+
+  const vm = new Constructor({
+    propsData: {
+      loading: true
+    }
+  })
+
+  vm.$mount()
+
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+
+  expect(href).to.eq('#i-loading')
+
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+
+  const vm = new Constructor({
+    propsData: {
+      loading: true
+    }
+  })
+
+  vm.$mount()
+
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+
+  expect(href).to.eq('#i-loading')
+
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+      iconPosition: 'left'
+    }
+  })
+
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+
+  vm.$mount(div)
+
+  vm.$nextTick(() => {
+    let svgElement = vm.$el.querySelector('svg')
+    let {order} = getComputedStyle(svgElement)
+
+    expect(order).to.eq('1')
+
+    document.body.removeChild(vm.$el)
+    vm.$destroy()
+  })
+}
+
+{
+  const Constructor = Vue.extend(Button)
+
+  const vm = new Constructor({
+    propsData: {
+      icon: 'setting',
+      iconPosition: 'right'
+    }
+  })
+
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+
+  vm.$mount(div)
+
+  vm.$nextTick(() => {
+    let svgElement = vm.$el.querySelector('svg')
+    let {order} = getComputedStyle(svgElement)
+
+    expect(order).to.eq('2')
+
+    document.body.removeChild(vm.$el)
+    vm.$destroy()
+  })
+}
+
+{
+  const Constructor = Vue.extend(Button)
+
+  const vm = new Constructor({
+    propsData: {
+      loading: true
+    }
+  })
+
+  vm.$mount()
+
+  let spy = chai.spy(function () {})
+
+  vm.$on('click', spy)
+
+  const button = vm.$el
+
+  button.click()
+
+  expect(spy).to.have.been.called()
+
+  vm.$destroy()
+}
